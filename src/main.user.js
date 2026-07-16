3f9ccba2ff2df57469170bdbf573b161f6b75d9a
AnhLe <96159700+tony72255@users.noreply.github.com>
2026-07-16 11:47:07 +0700

    Create main.user.js

---
+++ src/main.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         Lotte Mart - Tampermonkey Script
+// @namespace    http://tampermonkey.net/
+// @version      1.0.1
+// @description  Automation cho gmd.lottemart.vn (update từ GitHub)
+// @author       Your Name
+// @match        https://gmd.lottemart.vn/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=lottemart.vn
+// @grant        GM_xmlhttpRequest
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_notification
+// @connect      gmd.lottemart.vn
+// @connect      raw.githubusercontent.com
+// @updateURL    https://raw.githubusercontent.com/yourname/tamper-scripts/main/src/main.user.js
+// @downloadURL  https://raw.githubusercontent.com/yourname/tamper-scripts/main/src/main.user.js
+// @supportURL   https://t.me/your_telegram_bot
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log('[Tampermonkey] Lotte Mart script loaded - Version 1.0.1');
+
+    // ==================== CODE CỦA BẠN BẮT ĐẦU TỪ ĐÂY ====================
+
+    // Ví dụ:
+    // if (window.location.href.includes('/pcm05')) {
+    //     // code xử lý trang mua hàng
+    // }
+
+    // ==================== KẾT THÚC CODE ====================
+
+})();
